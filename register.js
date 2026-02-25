c73357c85c51e52173b307a46175219491d459ed
roma2 <romlen28@vgs.nfk.no>
2026-02-25 09:32:56 +0100

    123

---
+++ register.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyACuqAPDJ5Rq1QqHDO7ninVG7eORVqRqd0",
+    authDomain: "loging-123-782df.firebaseapp.com",
+    projectId: "loging-123-782df",
+    storageBucket: "loging-123-782df.firebasestorage.app",
+    messagingSenderId: "733787245209",
+    appId: "1:733787245209:web:22e1189cdfcd864e3e8608"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
